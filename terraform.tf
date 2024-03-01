4fb1689e053e8c436872480a6c082a9d4993a10e
Werner Strydom <bloudraak@users.noreply.github.com>
2024-02-29 18:54:07 -0800

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,14 +1,6 @@
 terraform {
   required_version = ">= 1, < 2"
   required_providers {
-    github = {
-      source  = "integrations/github"
-      version = ">= 5, < 7"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3, < 4"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3, < 4"
@@ -16,10 +8,6 @@ terraform {
     azuread = {
       source  = "hashicorp/azuread"
       version = ">= 2, < 3"
-    }
-    onepassword = {
-      source  = "1Password/onepassword"
-      version = ">= 1, < 2"
     }
   }
 }
